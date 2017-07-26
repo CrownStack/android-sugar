395bbaa274b970c0e3e150634da70fb5d2130afd
pkvarnwal <pkvarnwal@gmail.com>
2017-07-26 14:35:26 +0530

    Open gallery to choose image

---
+++ Gallery.java
@@ -0,0 +1,24 @@
+
+package com.e2cc.sugar;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import static android.content.Intent.ACTION_GET_CONTENT;
+
+public class Gallery {
+
+    private final Activity mActivity;
+
+    public Gallery(final Activity activity) {
+        this.mActivity = activity;
+    }
+
+    public Intent openPhoto(final int RequestCode) {
+        final Intent intent = new Intent(ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        mActivity.startActivityForResult(Intent.createChooser(intent, "Select Image"), RequestCode);
+
+        return intent;
+    }
+}
