6313e492e0d4d22502a1b87334094f2097a33f46
pkvarnwal <pkvarnwal@gmail.com>
2017-07-26 14:42:28 +0530

    To show progress dialog

---
+++ ProgressHelper.java
@@ -0,0 +1,22 @@
+package com.e2cc.sugar;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.view.Window;
+
+public class ProgressHelper {
+
+    private static ProgressDialog dialog;
+
+    public static void start(Context context, String message) {
+        dialog = new ProgressDialog(context);
+        dialog.setMessage(message);
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setCancelable(false);
+        dialog.show();
+    }
+
+    public static void stop() {
+        if (dialog.isShowing()) dialog.dismiss();
+    }
+}
