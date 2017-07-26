0a85f3fb18f78035ef36c6c0822a97c6c6ce29f2
pkvarnwal <pkvarnwal@gmail.com>
2017-07-26 14:36:48 +0530

    Convert image to Bitmap

---
+++ ImageHelper.java
@@ -0,0 +1,23 @@
+package com.e2cc.sugar;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+
+import java.io.IOException;
+
+public class ImageHelper {
+
+    public static Bitmap getBitmapFromGalleryIntent(Context context, Intent data) {
+        if (data != null) {
+            try {
+                return MediaStore.Images.Media.getBitmap(context.getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+}
