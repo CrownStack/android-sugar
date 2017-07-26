c71635c65b2ea19c98c6ef7a92745b74f294b4ee
pkvarnwal <pkvarnwal@gmail.com>
2017-07-26 14:51:44 +0530

    Refactor code style

--- NetworkHelper.java
+++ NetworkHelper.java
@@ -1,14 +1,13 @@
-package com.e2cc.sugar;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
 
 public class NetworkHelper {
 
-    private static ConnectivityManager _connectivityManager;
+    private static ConnectivityManager sConnectivityManager;
 
     public static boolean IsOn(final Context ctx) {
-        if (_connectivityManager == null) _connectivityManager = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
-        return _connectivityManager.getActiveNetworkInfo() != null && _connectivityManager.getActiveNetworkInfo().isAvailable() && _connectivityManager.getActiveNetworkInfo().isConnectedOrConnecting();
+        if (sConnectivityManager == null) sConnectivityManager = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return sConnectivityManager.getActiveNetworkInfo() != null && sConnectivityManager.getActiveNetworkInfo().isAvailable() && sConnectivityManager.getActiveNetworkInfo().isConnectedOrConnecting();
     }
 }
