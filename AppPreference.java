b0d884af6b9cab4472d15c051232698deb9dfd73
ashutoshsingh33 <aaa.singh33@gmail.com>
2017-09-11 15:16:03 +0530

    Refactor : AppPreferance : add methods

---
+++ AppPreference.java
@@ -0,0 +1,44 @@
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class AppPreference {
+
+    private static final String APP_SHARED_PREFERENCE = "Apps_Preferences";
+    private static SharedPreferences sSharedPreferences;
+    private static AppPreference sAppPreference;
+    private SharedPreferences.Editor mEditor;
+
+    private AppPreference(Context mContext) {
+        sSharedPreferences = mContext.getSharedPreferences(APP_SHARED_PREFERENCE, Context.MODE_PRIVATE);
+        mEditor = sSharedPreferences.edit();
+    }
+
+    public static AppPreference getAppPreference(Context mContext) {
+        if (sAppPreference == null) sAppPreference = new AppPreference(mContext);
+
+        return sAppPreference;
+    }
+
+    public void putString(String key, String value) {
+        mEditor.putString(key, value);
+        mEditor.apply();
+    }
+    
+    public String getString(String key) {
+       return sSharedPreferences.getString(key, "");
+    }
+
+    public void putBoolean(String key, boolean value) {
+        mEditor.putBoolean(key, value);
+        mEditor.apply();
+    }
+
+    public Boolean getBoolean(String key) {
+        return sSharedPreferences.getBoolean(key, false);
+    }
+
+    public void remove(String key) {
+        mEditor.remove(key);
+        mEditor.apply();
+    }
+}
