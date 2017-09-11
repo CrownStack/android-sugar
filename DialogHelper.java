79b2a97c7876286493f37b64aee3c3669e0a6671
ashutoshsingh33 <aaa.singh33@gmail.com>
2017-09-11 16:24:41 +0530

    Refactor : DialogHelper : improve code styling

--- DialogHelper.java
+++ DialogHelper.java
@@ -1,19 +1,6 @@
 import android.app.Activity;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.TextView;
-
-import com.intonate.R;
-import com.intonate.activity.ResultActivity;
-import com.intonate.font.FontHelper;
 
 public class DialogHelper {
 
